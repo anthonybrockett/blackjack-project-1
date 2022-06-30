fc5e3b5f303164c47049a551673ef6381bba3078
anthonybrockett <ajbrockett1991@gmail.com>
2022-06-29 20:45:29 -0700

    Update Pseudocode and Wireframe

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*----- constants -----*/
+const blackjack = 21; //UPDATE LATER
+
+/*----- app's state (variables) -----*/
+let bank;
+let dealerHand;
+let dealerHandTotal;
+let playerHand;
+let playerHandTotal;
+let cardsUsed;
+let currentHandStatus;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+function initialize() {
+
+};
+
+function render() {
+
+};
+
+function placeBet() {
+
+};
+
+function hit() {
+
+};
+
+function stay() {
+
+};
+
+function surrender() {
+
+};
