df2bb481c470fc1bff46e90a9d84636998328581
anthonybrockett <ajbrockett1991@gmail.com>
2022-07-01 15:05:33 -0700

    Update Render Bank and Render Bet

--- js/main.js
+++ js/main.js
@@ -3,26 +3,59 @@ const blackjack = 21; //UPDATE LATER
 
 /*----- app's state (variables) -----*/
 let bank;
+let bet;
 let dealerHand;
 let dealerHandTotal;
 let playerHand;
 let playerHandTotal;
-let cardsUsed;
 let currentHandStatus;
 /*----- cached element references -----*/
+const bankEl = document.getElementById('bank');
+const betEl = document.getElementById('current-bet');
+
 /*----- event listeners -----*/
+document.getElementById('bet-buttons').addEventListener('click', handleBet);
 /*----- functions -----*/
 
-function initialize() {
+initialize();
 
+function initialize() {
+    bank = 1000;
+    bet = 0;
+    dealerHand = [];
+    dealerHandTotal = 0;            
+    playerHand = [];
+    playerHandTotal = 0;
+    currentHandStatus = '';
+    render();
 };
 
 function render() {
-
+    renderBank();
+    renderBet();
 };
 
-function placeBet() {
+function renderBank() {
+    return bankEl.innerHTML = `Bank: ${bank}`;
+};
 
+function handleBet (evt) {
+//     // Guards
+//     // if (
+//     //     evt.target.tagName !== 'BUTTON' ||
+//     //     currentHandStatus !== ''
+//     // ) return;
+    if (evt.target.innerHTML === 'Reset') {
+        bet = 0;
+    } else {
+        bet += parseInt(evt.target.innerHTML);
+    };
+    render();
+    };
+
+function renderBet() {
+    console.log(bet, 'bet');
+    betEl.innerHTML = `Current Bet: ${bet}`;
 };
 
 function hit() {
